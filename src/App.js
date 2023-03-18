bd1f28a898bf5d3bdd8b1f75988aab2be350d22d
unknown <sharma.anurag1995@gmail.com>
2023-03-18 23:46:04 +0530

    added dark mode + code formatting

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./component/Navbar";
 import Textform from "./component/Textform";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#061d42";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
   return (
     <>
-      <Navbar title="Text Editor" about="About Us" />
+      <Navbar
+        title="Text Editor"
+        about="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
       <div className="container my-3">
-        <Textform heading="Enter your text in the below box:" />
+        <Textform heading="Enter your text in the below box:" mode={mode} />
       </div>
     </>
   );
--- src/component/Textform.js
+++ src/component/Textform.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 
-export default function Textform(prop) {
+export default function Textform(props) {
   const [text, setText] = useState("Enter your text here.");
   const handleUpperCase = () => {
     setText(text.toUpperCase());
+  };
+
+  const handleLowerCase = () => {
+    setText(text.toLowerCase());
   };
 
   const handleTextBoxChange = (event) => {
@@ -12,18 +15,35 @@ export default function Textform(prop) {
   };
 
   return (
-    <div className="mb-3">
-      <h1>{prop.heading}</h1>
-      <textarea
-        className="form-control"
-        value={text}
-        onChange={handleTextBoxChange}
-        id="exampleFormControlTextarea1"
-        rows="3"
-      ></textarea>
-      <button className="btn btn-primary my-4" onClick={handleUpperCase}>
-        Click to convert to Uppercase
-      </button>
-    </div>
+    <>
+      <div
+        className="container mb-3"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
+        <h1>{props.heading}</h1>
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleTextBoxChange}
+          id="exampleFormControlTextarea1"
+          rows="3"
+          style={{ backgroundColor: props.mode === "light" ? "white" : "grey" }}
+        ></textarea>
+        <button className="btn btn-primary my-4 mx-2" onClick={handleUpperCase}>
+          Click to convert to Uppercase
+        </button>
+        <button className="btn btn-primary my-4 mx-2" onClick={handleLowerCase}>
+          Click to convert to Lowercase
+        </button>
+      </div>
+      <div
+        className="container"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
+        <h2>Your text Stats: </h2>
+        <p>Number of words: {text.split(" ").length}</p>
+        <p>Total character counts: {text.length}</p>
+      </div>
+    </>
   );
 }

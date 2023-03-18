802af4cd882965d25057852165afffd7f1aa71f5
unknown <sharma.anurag1995@gmail.com>
2023-03-18 11:47:13 +0530

    created first state + added form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './component/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Textform from "./component/Textform";
 
 function App() {
   return (
     <>
-    <Navbar title = "Text Editor" about="About Us"/>
+      <Navbar title="Text Editor" about="About Us" />
+      <div className="container my-3">
+        <Textform heading="Enter your text in the below box:" />
+      </div>
     </>
   );
 }
